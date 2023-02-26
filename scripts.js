b5b294394f7dca25f311ceaaf76fe50cfc4c4c43
citizenkehoe <citizenkehoe@gmail.com>
2023-02-25 22:27:08 -0800

    Build working proof-of-concept for 16x16 grid

---
+++ scripts.js
@@ -0,0 +1,6 @@
+for (let i = 0; i < 256; i++) {
+    let etchContainer = document.getElementById('contianer');
+    let etchPoint = document.createElement('div');
+    etchPoint.classList.add('square');
+    etchContainer.appendChild(etchPoint);
+};
