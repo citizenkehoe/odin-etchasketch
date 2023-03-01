f0cfd98b55d76bad0bec2c06e317a8fc17688443
citizenkehoe <citizenkehoe@gmail.com>
2023-02-28 19:47:44 -0800

    Add logic to "Make new etch" button

--- scripts.js
+++ scripts.js
@@ -1,33 +1,56 @@
-let magicNum = 16;
-let totalSquares = magicNum ** 2;
+// connect to html elements
 let etchContainer = document.getElementById('etch-contianer');
 let resetButton = document.getElementById('clear-sketch');
 let newEtchButton = document.getElementById('make-etch');
 
-
-for (let i = 0; i < totalSquares; i++) {
-    let etchPoint = document.createElement('div');
-    etchPoint.classList.add('square');
-    etchContainer.appendChild(etchPoint);
+// create drawing area & auto-load
+function makeDrawArea(x = 16) {
+    for (let i = 0; i < x ** 2; i++) {
+        let etchPoint = document.createElement('div');
+        etchPoint.classList.add('square');
+        etchContainer.appendChild(etchPoint);
+    };
+    etchContainer.style.gridTemplateColumns = `repeat(${x}, 1fr)`;
+    etchContainer.style.gridTemplateRows = `repeat(${x}, 1fr)`;
 };
+makeDrawArea();
 
-etchContainer.style.gridTemplateColumns = `repeat(${magicNum}, 1fr)`;
-etchContainer.style.gridTemplateRows = `repeat(${magicNum}, 1fr)`;
-
+// add 'etched' class
 function etchMe() {
     this.classList.add('etched');
 };
 
-let etchPoints = document.querySelectorAll('.square');
-etchPoints.forEach(etchPoint => {
-    etchPoint.addEventListener('pointerover', etchMe);
-});
-
-function clearSketch() {
-    console.log('whoa!');
+// attach 'etched' class to all created squares
+function makeEtchPoints() {
+    let etchPoints = document.querySelectorAll('.square');
     etchPoints.forEach(etchPoint => {
+        console.log('im here');
+        etchPoint.addEventListener('pointerover', etchMe);
+    });
+    return etchPoints;
+};
+makeEtchPoints();
+
+// clear drawing area & add to corresponding button
+function clearSketch() {
+    makeEtchPoints().forEach(etchPoint => {
         etchPoint.classList.remove('etched');
     });
 };
-
 resetButton.addEventListener('click', clearSketch);
+
+// create new drawing area & add to corresponding button
+function makeNewEtch() {
+    let input = prompt("How detailed of a sketch would you like to etch?", "Enter number between 1 and 100");
+    if (isNaN(input) || input < 1 || input > 100) {
+        alert('Error; please enter an number between 1 and 100.');
+    } else {
+        clearSketch();
+        while (etchContainer.firstChild) {
+            etchContainer.removeChild(etchContainer.firstChild);
+        }
+        makeDrawArea(input);
+        makeEtchPoints();
+    };
+};
+newEtchButton.addEventListener('click', makeNewEtch);
